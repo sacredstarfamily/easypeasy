ffe90ea8c99ad7d984ea5f881c901b482ddce797
sacredstarfamily <seeloveinfinite@gmail.com>
2023-03-07 17:56:07 -0800

    asdf
    asdf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ if (typeof window !== 'undefined') {
 export default function App({ Component, pageProps }: AppProps) {
   const addPayPal = ()=>{
     const script = document.createElement("script");
-    script.src = "https://www.paypal.com/sdk/js?client-id=AV7uZUGRxkHDD6OrogbfBbD24sqxtDqRxWKQCgGzRodAg8s8FuU8yCXdaN7aH-5gFDTxW-cHS6Dmq2N-&enable-funding=venmo&currency=USD&vault=true&intent=subscription";
+    script.src = "https://www.paypal.com/sdk/js?client-id=AdytsnuuQr8Ywn8oN1gbheEBaWJg688s0__2LiDIeWojycWBnbtfAUWUPVQhSCLQuPxO7Z1e9Mx0V9eE&enable-funding=venmo&currency=USD&vault=true&intent=subscription";
     script.type = "text/javascript";
     script.async = true;
     document.body.appendChild(script);
