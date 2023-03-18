dd0ab3cc0df33abe8d90da29e67ef5ffc6fd6a22
sacredstarfamily <seeloveinfinite@gmail.com>
2023-03-17 23:03:33 -0700

    asfd
    asf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -204,7 +204,19 @@ export default function Home() {
 				vault: true,
 			}}
 		>
-        
+        <PayPalButtons createSubscription={(data, actions) => {
+			return actions.subscription
+				.create({
+					plan_id: "P-3RX065706M3469222L5IFM4I",
+				})
+				.then((orderId) => {
+					// Your code here after create the order
+					return orderId;
+				});
+		}}
+		style={{
+			label: "subscribe",
+		}}></PayPalButtons>
           </PayPalScriptProvider>
         </div>
       
